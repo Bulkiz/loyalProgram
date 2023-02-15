2eb73638987df32f684973e708a8cab16d611c00
Bulkiz <vladislav.nedyalkov@nevexis.com>
2023-02-15 15:54:48 +0200

    fix

--- src/main/java/com/example/loyalProgram/config/Config.java
+++ src/main/java/com/example/loyalProgram/config/Config.java
@@ -3,7 +3,6 @@ package com.example.loyalProgram.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
 
 @Configuration
 public class Config {
--- src/main/java/com/example/loyalProgram/controllers/SaleController.java
+++ src/main/java/com/example/loyalProgram/controllers/SaleController.java
@@ -3,7 +3,11 @@ package com.example.loyalProgram.controllers;
 import com.example.loyalProgram.DTOs.ClientDTO;
 import com.example.loyalProgram.DTOs.MerchantDTO;
 import com.example.loyalProgram.DTOs.TierDTO;
+import com.example.loyalProgram.entities.Client;
+import com.example.loyalProgram.entities.Merchant;
+import com.example.loyalProgram.entities.Tier;
 import com.example.loyalProgram.services.AddingService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,23 +18,29 @@ import java.util.List;
 public class SaleController {
 
     @Autowired AddingService addingService;
+    @Autowired ModelMapper modelMapper;
     @PostMapping("/add")
     public MerchantDTO addMerchant(@RequestBody MerchantDTO merchantDTO){
-        return addingService.addMerchant(merchantDTO);
+        return modelMapper.map(addingService.addMerchant(modelMapper.map(merchantDTO, Merchant.class)), MerchantDTO.class);
     }
 
     @PostMapping("addTiers/{merchantId}")
     public List<TierDTO> addTiersToMerchant(@PathVariable Integer merchantId, @RequestBody List<TierDTO> tierDTOS){
-        return addingService.addTiers(merchantId, tierDTOS);
+        return addingService.addTiers(merchantId, tierDTOS.parallelStream().
+                map(tier -> modelMapper.map(tier, Tier.class)).toList()).parallelStream().
+                map(tier -> modelMapper.map(tier, TierDTO.class)).toList();
     }
 
     @GetMapping("/allTiers")
     public List<TierDTO> findAllTiers(){
-        return addingService.findAllTiers();
+        return addingService.findAllTiers().parallelStream().
+                map(tier -> modelMapper.map(tier, TierDTO.class)).toList();
     }
 
-    @PostMapping("addClients/")
+    @PostMapping("/addClients")
     public List<ClientDTO> addClients(@RequestBody List<ClientDTO> clientDTOs){
-        return addingService.addClients(clientDTOs);
+        return addingService.addClients(clientDTOs.parallelStream().
+                map(clientDTO -> modelMapper.map(clientDTO, Client.class)).toList()).parallelStream().
+                map(client -> modelMapper.map(client, ClientDTO.class)).toList();
     }
 }
