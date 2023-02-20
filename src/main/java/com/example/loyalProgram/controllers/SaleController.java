96979aa1647be5c521d3ecf9934d44d84641c834
Blagovest <blagovest.kirilov@nevexis.com>
2023-02-20 13:28:57 +0200

    update status by date

--- src/main/java/com/example/loyalProgram/controllers/SaleController.java
+++ src/main/java/com/example/loyalProgram/controllers/SaleController.java
@@ -2,6 +2,7 @@ package com.example.loyalProgram.controllers;
 
 import com.example.loyalProgram.saleModule.DTOs.SaleDTO;
 import com.example.loyalProgram.saleModule.DTOs.SaleMapper;
+import com.example.loyalProgram.services.RedeemPointsService;
 import com.example.loyalProgram.services.SaleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,9 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class SaleController {
     @Autowired private SaleService saleService;
     @Autowired private SaleMapper saleMapper;
+    @Autowired private RedeemPointsService redeemPointsService;
 
     @PostMapping
     public void makeSell(@RequestBody SaleDTO saleDTO){
         saleService.makeSale(saleMapper.saleMapper(saleDTO));
+    }
+    @PostMapping("/updateByDate")
+    public void updateByDate(){
+        redeemPointsService.updateStatusByDate();
     }
 }
---
+++ src/main/java/com/example/loyalProgram/services/RedeemPointsService.java
@@ -0,0 +1,6 @@
+package com.example.loyalProgram.services;
+
+public interface RedeemPointsService {
+    void updateStatusByDate();
+    //void updateCardBalanceByStatus(List<CardHistory> cardHistoryList);
+}
