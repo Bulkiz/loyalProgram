24fcfa500dc21921a368fa3595a79d6e2aa42e12
Bulkiz <vladislav.nedyalkov@nevexis.com>
2023-02-24 16:08:42 +0200

    ku

---
+++ src/test/java/com/example/loyalProgram/TestMakeSaleUpgradeClientTier.java
@@ -0,0 +1,112 @@
+package com.example.loyalProgram;
+
+import com.example.loyalProgram.clientModule.entities.Client;
+import com.example.loyalProgram.clientModule.repositories.CardRepository;
+import com.example.loyalProgram.enums.LoyalProgramType;
+import com.example.loyalProgram.merchantModule.entities.LoyalProgram;
+import com.example.loyalProgram.merchantModule.entities.Merchant;
+import com.example.loyalProgram.merchantModule.entities.Tier;
+import com.example.loyalProgram.merchantModule.services.impl.AddingServiceImpl;
+import com.example.loyalProgram.saleModule.entities.Sale;
+import com.example.loyalProgram.saleModule.services.SaleService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.mockito.Mockito.mock;
+
+@SpringBootTest
+public class TestMakeSaleUpgradeClientTier {
+    @Autowired
+    SaleService saleService;
+    @Autowired
+    AddingServiceImpl addingService;
+    Merchant testMerchant = mock(Merchant.class);
+    Sale testSale = mock(Sale.class);
+    Client testClient = mock(Client.class);
+    LoyalProgram testLoyalProgram = mock(LoyalProgram.class);
+    LoyalProgram testLoyalProgramDiscount = mock(LoyalProgram.class);
+    Tier testTier = mock(Tier.class);
+    Tier secondTier = mock(Tier.class);
+
+    @Autowired
+    CardRepository cardRepository;
+
+    @BeforeEach
+    public void setUp() {
+        testMerchant = Merchant.builder().name("TestMerchant").build();
+        addingService.addMerchant(testMerchant);
+
+        testLoyalProgramDiscount = LoyalProgram.builder()
+                .name("TestLoyalProgram")
+                .priority(10)
+                .discountPercentage(BigDecimal.TEN)
+                .type(LoyalProgramType.DISCOUNT)
+                .build();
+
+
+        testLoyalProgram = LoyalProgram.builder()
+                .name("TestLoyalProgram")
+                .priority(30)
+                .discountPercentage(BigDecimal.TEN)
+                .type(LoyalProgramType.ADD_POINTS)
+                .build();
+
+        List<LoyalProgram> testListLoyalProgram = new LinkedList<>();
+        testListLoyalProgram.add(testLoyalProgramDiscount);
+        testListLoyalProgram.add(testLoyalProgram);
+
+        testTier = Tier.builder()
+                .name("TestTier")
+                .merchant(testMerchant)
+                .loyalPrograms(testListLoyalProgram)
+                .tierAmount(BigDecimal.valueOf(100))
+                .build();
+
+        secondTier = Tier.builder()
+                .name("TestTier")
+                .merchant(testMerchant)
+                .loyalPrograms(testListLoyalProgram)
+                .tierAmount(BigDecimal.valueOf(200))
+                .build();
+
+        List<Tier> testListTier = new LinkedList<>();
+
+        testListTier.add(testTier);
+        testListTier.add(secondTier);
+
+        addingService.addTiers(testMerchant.getId(), testListTier);
+
+        testClient = Client.builder()
+                .name("TestClient")
+                .merchant(testMerchant)
+                .tier(testTier)
+                .birthday(LocalDate.now())
+                .amountSpend(BigDecimal.ZERO)
+                .build();
+
+        List<Client> testListClients = new LinkedList<>();
+        testListClients.add(testClient);
+
+        addingService.addClients(testListClients);
+
+        testSale = Sale.builder()
+                .client(testClient)
+                .merchant(testMerchant)
+                .originalPrice(BigDecimal.valueOf(100))
+                .summaryPrice(BigDecimal.valueOf(100))
+                .build();
+    }
+
+    @Test
+    public void testMakeSale() {
+
+    }
+
+}
